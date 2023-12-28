09cfccc6f72d4904559e6201b9f72d29445c2e36
Zemogus <kiyonnxperiaz@gmail.com>
2023-12-28 13:56:58 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Nterm
+
+A library that makes developing games for the terminal easier by providing a canvas-like interface for optimised drawing to the terminal, and captures and handles key presses, releases, and holds.
+This library is currently being developed solely to support [Budget Tetris](https://github.com/Zemogus/Budget-Tetris), but may be used as a standalone library as well.
---
+++ build.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib = b.addStaticLibrary(.{
+        .name = "nterm",
+        .root_source_file = .{ .path = "src/root.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // Expose the library root
+    _ = b.addModule("nterm", .{
+        .source_file = .{ .path = "src/root.zig" },
+    });
+
+    b.installArtifact(lib);
+
+    // Add test step
+    const lib_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/root.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_lib_tests = b.addRunArtifact(lib_tests);
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_tests.step);
+}
